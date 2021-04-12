1497edd9d2d54709b7bc1d489346bec866600aa2
aurore.bussalb <aurore.bussalb@mensiatech.com>
2021-04-12 16:04:07 +0200

    add events parameter in function

--- make_epochs.py
+++ make_epochs.py
@@ -4,7 +4,7 @@ import json
 import mne
 import numpy as np
 
-def make_epochs(raw):
+def make_epochs(raw, events):
 
     # Convert tsv file into 
     array_events = np.loadtxt(fname=events, delimiter="\t")
