e3e3ee9c6536c435e8a09eadaf9bd2ece88326b7
aurore.bussalb <aurore.bussalb@mensiatech.com>
2021-04-12 16:32:30 +0200

    convert floats to ints

--- make_epochs.py
+++ make_epochs.py
@@ -8,8 +8,8 @@ def make_epochs(raw, events):
 
     # Convert tsv file into 
     array_events = np.loadtxt(fname=events, delimiter="\t")
+    array_events.astype(int)
     print(array_events)
-    array_events.ravel()
 
     # create epochs
     epoched_data = mne.Epochs(raw, array_events)
