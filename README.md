af1ff1201b54d1fcb9b55e8213af140fa531f04c
aurore.bussalb <aurore.bussalb@mensiatech.com>
2021-04-16 14:58:48 +0200

    make it compatible with BL

--- README.md
+++ README.md
@@ -1,2 +1,94 @@
 # app-make-epochs
-Create epochs in MEG data
+
+This is a draft of a future Brainlife App using MNE Python to epoch the data ([`mne.Epochs`](hhttps://mne.tools/stable/generated/mne.Epochs.html).
+
+# app-make-epochs documentation
+
+1) Epoch the data. 
+2) Input files are:
+    * a MEG file in `.fif` format,
+    * an events file in `.tsv` format,
+    * an optional fine calibration file in `.dat`,
+    * an optional crosstalk compensation file in `.fif`,
+    * an optional head position file in `.pos`,
+    * an optional destination file in `.fif`,
+3) Input parameters are:
+    * `param_event_id`: `int`, `list of int`, optional, the id of the event to consider. Default is None.
+    * `param_tmin`: `float`, start time before event. Default is -0.2.
+    * `param_tmax`: `float`, end time after event. Default is 0.5.
+    * `param_baseline`: `tuple` of length 2, optional, the time interval to consider as “baseline” when applying baseline correction. Default is (None, 0).
+    * `param_picks`: `str`, `list`, `slice`, optional, channels to include. Default is None.
+    * `param_preload`: `bool`, load all epochs from disk when creating the object or wait before accessing each epoch. Default is False.
+    * `param_reject`: `dict`, optional, reject epochs based on peak-to-peak signal amplitude, i.e. the absolute difference between the lowest and the highest signal value. 
+       Default is None.
+    * `param_flat`: `dict`, optional, rejection parameters based on flatness of signal. Valid keys are 'grad', 'mag', 'eeg', 'eog', 'ecg'. 
+        The values are floats that set the minimum acceptable peak-to-peak amplitude. Default is None.
+    * `param_proj`: `bool` or `str`, apply SSP projection vectors. If proj is "delayed" and reject is not None the single epochs will be 
+        projected before the rejection decision, but used in unprojected state if they are kept. Default is True.
+    * `param_decim`: `int`, factor by which to subsample the data. Default is 1.
+    * `param_reject_tmin`: `scalar`, optional, start of the time window used to reject epochs (with the default None, the window will start with tmin).
+    * `param_reject_tmax`: `scalar` or None, end of the time window used to reject epochs (with the default None, the window will end with tmax).  
+    * `param_detrend`: `int`, optional, if 0 or 1, the data channels (MEG and EEG) will be detrended when loaded. 0 is a constant (DC) detrend, 1 
+        is a linear detrend. Default is None (no detrending).
+    * `param_on_missing`: `str`, what to do if one or several event ids are not found in the recording. Valid keys are 'raise', 'warn', or
+        'ignore'. Default is 'raise'.
+    * `param_reject_by_annotation`: `bool`, whether to reject based on annotations. If True, epochs overlapping with segments whose description 
+        begins with 'bad' are rejected. Default is True.
+    * `param_metadata`: instance of `pandas.DataFrame` or None
+        A pandas.DataFrame specifying metadata about each epoch. Default is None.
+    * `param_event_repeated`: `str`, how to handle duplicates in events[:, 0]. Can be 'error', to raise an error, ‘drop’ to only retain 
+        the row occurring first in the events, or 'merge' to combine the coinciding events (=duplicates) into a new event. Default is 'error'.
+      
+This list along with the default values correspond to the parameters of MNE Python version 0.22.0.
+
+4) Ouput file is an epoched MEG data in a .fif file
+
+### Authors
+- [Aurore Bussalb](aurore.bussalb@icm-institute.org)
+
+### Contributors
+- [Aurore Bussalb](aurore.bussalb@icm-institute.org)
+- [Maximilien Chaumon](maximilien.chaumon@icm-institute.org)
+
+### Funding Acknowledgement
+brainlife.io is publicly funded and for the sustainability of the project it is helpful to Acknowledge the use of the platform. We kindly ask that you acknowledge the funding below in your code and publications. Copy and past the following lines into your repository when using this code.
+
+[![NSF-BCS-1734853](https://img.shields.io/badge/NSF_BCS-1734853-blue.svg)](https://nsf.gov/awardsearch/showAward?AWD_ID=1734853)
+[![NSF-BCS-1636893](https://img.shields.io/badge/NSF_BCS-1636893-blue.svg)](https://nsf.gov/awardsearch/showAward?AWD_ID=1636893)
+[![NSF-ACI-1916518](https://img.shields.io/badge/NSF_ACI-1916518-blue.svg)](https://nsf.gov/awardsearch/showAward?AWD_ID=1916518)
+[![NSF-IIS-1912270](https://img.shields.io/badge/NSF_IIS-1912270-blue.svg)](https://nsf.gov/awardsearch/showAward?AWD_ID=1912270)
+[![NIH-NIBIB-R01EB029272](https://img.shields.io/badge/NIH_NIBIB-R01EB029272-green.svg)](https://grantome.com/grant/NIH/R01-EB029272-01)
+
+### Citations
+1. Avesani, P., McPherson, B., Hayashi, S. et al. The open diffusion data derivatives, brain data upcycling via integrated publishing of derivatives and reproducible open cloud services. Sci Data 6, 69 (2019). [https://doi.org/10.1038/s41597-019-0073-y](https://doi.org/10.1038/s41597-019-0073-y)
+2. Taulu S. and Kajola M. Presentation of electromagnetic multichannel data: The signal space separation method. Journal of Applied Physics, 97 (2005). [https://doi.org/10.1063/1.1935742](https://doi.org/10.1063/1.1935742)
+3. Taulu S. and Simola J. Spatiotemporal signal space separation method for rejecting nearby interference in MEG measurements. Physics in Medicine and Biology, 51 (2006). [https://doi.org/10.1088/0031-9155/51/7/008](https://doi.org/10.1088/0031-9155/51/7/008)
+
+
+## Running the App 
+
+### On Brainlife.io
+
+This App is still private.
+
+### Running Locally (on your machine)
+
+1. git clone this repo
+2. Inside the cloned directory, create `config.json` with the same keys as in `config.json.example` but with paths to your input 
+   files and values of the input parameters. For instance:
+
+```json
+{
+  "fif": "rest1-raw.fif"
+}
+```
+
+3. Launch the App by executing `main`
+
+```bash
+./main
+```
+
+## Output
+
+The output file is .fif file.
--- make_epochs.py
+++ make_epochs.py
@@ -4,20 +4,81 @@ import json
 import mne
 import numpy as np
 import warnings
+import shutil
 import os 
 
-def make_epochs(raw, events):
+def make_epochs(raw, events_file, param_event_id, param_tmin, param_tmax, param_baseline,
+                param_picks, param_preload, param_reject, param_flat, param_proj, param_decim,
+                param_reject_tmin, param_reject_tmax, param_detrend, param_on_missing, param_reject_by_annotation,
+                param_metadata, param_event_repeated):
+	"""Creta epochs from matrix events contained in a .tsv file.
 
-    if events is not None:
-        # Convert tsv file into a numpy array of integers
-        array_events = np.loadtxt(fname=events, delimiter="\t")
-        events = array_events.astype(int)
+    Parameters
+    ----------
+    raw: instance of mne.io.Raw
+        Data from which the events will be extracted or created.
+    events_file: str
+        Path to the .tsv events file containing the matrix of events.
+    param_event_id: int, list of int, or None
+        The id of the event to consider. Default is None.
+    param_tmin: float
+        Start time before event. Default is -0.2.
+    param_tmax: float
+        End time after event. Default is 0.5.
+    param_baseline: tuple of length 2 or None
+        The time interval to consider as “baseline” when applying baseline correction. 
+        Default is (None, 0).
+    param_picks: str, list, slice, or None
+        Channels to include. Default is None.
+    param_preload: bool
+        Load all epochs from disk when creating the object or wait before accessing each epoch.
+        Default is False.
+    param_reject: dict or None
+        Reject epochs based on peak-to-peak signal amplitude, i.e. the absolute difference between 
+        the lowest and the highest signal value. Default is None.
+    param_flat: dict or None
+        Rejection parameters based on flatness of signal. Valid keys are 'grad', 'mag', 'eeg', 'eog', 'ecg'. 
+        The values are floats that set the minimum acceptable peak-to-peak amplitude. Default is None.
+    param_proj: bool or str
+        Apply SSP projection vectors. If proj is ‘delayed’ and reject is not None the single epochs will be 
+        projected before the rejection decision, but used in unprojected state if they are kept. Default is True.
+    param_decim: int
+        Factor by which to subsample the data. Default is 1.
+    param_reject_tmin: scalar or None
+        Start of the time window used to reject epochs (with the default None, the window will start with tmin).
+    param_reject_tmax: scalar or None
+        End of the time window used to reject epochs (with the default None, the window will end with tmax).  
+    param_detrend: int or None
+        If 0 or 1, the data channels (MEG and EEG) will be detrended when loaded. 0 is a constant (DC) detrend, 1 
+        is a linear detrend. None (default) is no detrending.
+    param_on_missing: str
+        What to do if one or several event ids are not found in the recording. Valid keys are ‘raise’, ‘warn’, or
+        ‘ignore’. Default is ‘raise’.
+    param_reject_by_annotation: bool
+        Whether to reject based on annotations. If True (default), epochs overlapping with segments whose description 
+        begins with 'bad' are rejected. 
+    param_metadata: instance of pandas.DataFrame or None
+        A pandas.DataFrame specifying metadata about each epoch. Default is None.
+    param_event_repeated: str
+        How to handle duplicates in events[:, 0]. Can be 'error' (default), to raise an error, ‘drop’ to only retain 
+        the row occurring first in the events, or 'merge' to combine the coinciding events (=duplicates) into a new event.
 
-    else:
-        events = mne.find_events(raw)
+    Returns
+    -------
+    epoched_data: instance of mne.Epochs
+        The epoched data.
+    """
+
+    # Convert tsv file into a numpy array of integers
+    array_events = np.loadtxt(fname=events_file, delimiter="\t")
+    events = array_events.astype(int)
 
     # Create epochs from events
-    epoched_data = mne.Epochs(raw, events)
+    epoched_data = mne.Epochs(raw, events, event_id=param_event_id, tmin=param_tmin, tmax=param_tmax, baseline=param_baseline,
+	                          picks=param_picks, preload=param_preload, reject=param_reject, flat=param_flat, proj=param_proj, 
+	                          decim=param_decim, reject_tmin=param_reject_tmin, reject_tmax=param_reject_tmax, detrend=param_detrend, 
+	                          on_missing=param_on_missing, reject_by_annotation=param_reject_by_annotation,
+	                          metadata=param_metadata, event_repeated=param_event_repeated)
 
     # Save file
     epoched_data.save("out_dir_make_epochs/meg.fif", overwrite=True)
@@ -44,18 +105,83 @@ def main():
     # Test if events file exist
     if os.path.exists(events_file) is False:
         events_file = None
-        user_warning_message = f'You are going to create epochs from events ' \
-                               f'contained in your fif file.'
-        warnings.warn(user_warning_message)
-        dict_json_product['brainlife'].append({'type': 'info', 'msg': user_warning_message})
+        value_error_message = f'You need to provide events.tsv to make epochs. ' \
+                              f'Please use the app-get-events to create such file.'
+        # Raise exception
+        raise ValueError(value_error_message)
     else:
-        user_warning_message = f'You are going to create epochs from events ' \
-                               f'created beforehand.'
-        warnings.warn(user_warning_message)
-        dict_json_product['brainlife'].append({'type': 'info', 'msg': user_warning_message})
+    	shutil.copy2(events_file, 'out_dir_make_epochs/events.tsv')  # required to run a pipeline on BL
+
+    # Read the crosstalk file
+    cross_talk_file = config.pop('crosstalk')
+    if os.path.exists(cross_talk_file) is True:
+        shutil.copy2(cross_talk_file, 'out_dir_make_epochs/crosstalk_meg.fif')  # required to run a pipeline on BL
+
+    # Read the calibration file
+    calibration_file = config.pop('calibration')
+    if os.path.exists(calibration_file) is True:
+        shutil.copy2(calibration_file, 'out_dir_make_epochs/calibration_meg.dat')  # required to run a pipeline on BL
+
+    # Read destination file 
+    destination_file = config.pop('destination')
+    if os.path.exists(destination_file) is True:
+        shutil.copy2(destination_file, 'out_dir_make_epochs/destination.fif')  # required to run a pipeline on BL
+
+    # Read head pos file
+    head_pos = config.pop('headshape')
+    if os.path.exists(head_pos) is True:
+        shutil.copy2(head_pos, 'out_dir_make_epochs/headshape.pos')  # required to run a pipeline on BL
+
+    # Check for None parameters
+
+    # event id
+    if config['param_event_id'] == "":
+        config['param_event_id'] = None  # when App is run on Bl, no value for this parameter corresponds to ''
+
+    # Baseline
+    if config['param_baseline'] == "":
+        config['param_baseline'] = None  # when App is run on Bl, no value for this parameter corresponds to ''
+
+    # picks
+    if config['param_picks'] == "":
+        config['param_picks'] = None  # when App is run on Bl, no value for this parameter corresponds to ''    
+
+    # reject
+    if config['param_reject'] == "":
+        config['param_reject'] = None  # when App is run on Bl, no value for this parameter corresponds to '' 
+
+    # flat
+    if config['param_flat'] == "":
+        config['param_flat'] = None  # when App is run on Bl, no value for this parameter corresponds to '' 
+
+    # reject tmin
+    if config['param_reject_tmin'] == "":
+        config['param_reject_tmin'] = None  # when App is run on Bl, no value for this parameter corresponds to '' 
+
+    # reject tmax
+    if config['param_reject_tmax'] == "":
+        config['param_reject_tmax'] = None  # when App is run on Bl, no value for this parameter corresponds to ''
+
+    # reject detrend
+    if config['param_detrend'] == "":
+        config['param_param_detrend'] = None  # when App is run on Bl, no value for this parameter corresponds to ''
+
+    # reject metadata
+    if config['param_metadata'] == "":
+        config['param_metadata'] = None  # when App is run on Bl, no value for this parameter corresponds to ''
+
+    # Define kwargs
+
+    # Delete headshape key from config file
+    del config['events']
+
+    # Delete keys values in config.json when this app is executed on Brainlife
+    if '_app' and '_tid' and '_inputs' and '_outputs' in config.keys():
+        del config['_app'], config['_tid'], config['_inputs'], config['_outputs'] 
+    kwargs = config  
 
     # Epoch data
-    epoched_data = make_epochs(raw, events_file)
+    epoched_data = make_epochs(raw, events_file, **kwargs)
 
     # Success message in product.json    
     dict_json_product['brainlife'].append({'type': 'success', 'msg': 'Data was successfully epoched.'})
